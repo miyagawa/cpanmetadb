b5cc6cffc2943d218ac4041a01057bc89250e017
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-03-04 19:31:52 -0800

    some code cleanup

--- main.py
+++ main.py
@@ -2,20 +2,12 @@
 
 from google.appengine.ext import webapp, db
 from google.appengine.ext.webapp import util
-from google.appengine.api import urlfetch
+from google.appengine.api import urlfetch, users
 from google.appengine.api.labs import taskqueue
-from google.appengine.api import users
-import re
-import gzip
-import StringIO
-import logging
-import urllib
-import yaml
+import re, gzip, StringIO, logging, urllib, yaml
 
 def work_queue_only(func):
   """Decorator that only allows a request if from cron job, task, or an admin.
-
-  Also allows access if running in development server environment.
 
   Args:
     func: A webapp.RequestHandler method.
